8606cd47426772798bb0c63bf600ec31118652ac
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-01-25 06:21:06 +0530

    chore: logout button redirects to /login page on success

--- src/features/auth/components/logout-button.tsx
+++ src/features/auth/components/logout-button.tsx
@@ -2,7 +2,19 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export const LogoutButton = () => {
-  return <Button onClick={() => authClient.signOut()}>Logout</Button>;
+  const router = useRouter();
+  return (
+    <Button
+      onClick={() =>
+        authClient.signOut({
+          fetchOptions: { onSuccess: () => router.push("/login") },
+        })
+      }
+    >
+      Logout
+    </Button>
+  );
 };
