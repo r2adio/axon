323e8ee527636b029ddbea0a957708d9c9883ae6
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-01-26 23:14:27 +0530

    feat(ui): add layout for auth pages (login, signup)

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -4,11 +4,7 @@ import { requireUnauth } from "@/lib/auth-utils";
 const Page = async () => {
   await requireUnauth(); // prevents user to access login page, by manually changing the url
 
-  return (
-    <div>
-      <RegisterForm />
-    </div>
-  );
+  return <RegisterForm />;
 };
 
 export default Page;
