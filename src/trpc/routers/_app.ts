bdcd3ac196a09889f408c43e3e1889dca572d17a
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-01-27 21:59:40 +0530

    fix(trpc): update getWorkflows return value to query all user workflows
    NOTE:
    - this commit temporarily changes the getWorkflows function in the trpc router to return workflows for all users,
      instead of just the authenticated or current user.
    - this is a stopgap measure to facilitate testing and development until proper user authentication and authorization mechanisms are implemented.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,13 @@
 import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/prisma";
 export const appRouter = createTRPCRouter({
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
+  getWorkflows: protectedProcedure.query(() => {
     // console.log({ userId: ctx.auth.user.id });
 
-    return prisma.workflow.findMany({
-      where: { id: ctx.auth.user.id }, // query db users, which belongs to current user id
-    });
+    // return prisma.workflow.findMany({
+    //   where: { id: ctx.auth.user.id }, // query db users, which belongs to current user id
+    // });
+    return prisma.workflow.findMany();
   }),
 
   createWorkflow: protectedProcedure.mutation(() => {
