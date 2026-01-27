a8c79d35c76c4afec0993abc4e7e330a55ca186c
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-01-27 21:45:59 +0530

    feat(workflow): add initial workflows implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { LogoutButton } from "@/features/auth/components/logout-button";
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-export default async function Home() {
-  await requireAuth();
+export default function Home() {
+  // await requireAuth();
 
-  const data = await caller.getUsers();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    }),
+  );
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected page (login to access)
       <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,19 @@
 import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/prisma";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
     // console.log({ userId: ctx.auth.user.id });
 
-    return prisma.user.findMany({
+    return prisma.workflow.findMany({
       where: { id: ctx.auth.user.id }, // query db users, which belongs to current user id
+    });
+  }),
+
+  createWorkflow: protectedProcedure.mutation(() => {
+    return prisma.workflow.create({
+      data: {
+        name: "new workflow",
+      },
     });
   }),
 });
