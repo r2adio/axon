9a81e79e7f205776ccff7b387a2e6ed2b8a8572e
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-02-01 20:47:55 +0530

    feat(payments): add polar payment integration via better-auth plugin

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,3 +1,4 @@
+import { polarClient } from "@polar-sh/better-auth";
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient(); // function to create a auth client
+export const authClient = createAuthClient({ plugins: [polarClient()] }); // function to create a auth client
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,9 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // import { PrismaClient } from "@generated/prisma/client";
 import prisma from "@/lib/prisma";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   // database client
@@ -11,4 +13,19 @@ export const auth = betterAuth({
 
   // authentication methods
   emailAndPassword: { enabled: true, autoSignIn: true },
+
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [{ productId: "f81be8a8-45e1", slug: "pro" }],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
